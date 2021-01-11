6bc0851c59ad75ba3f09162b5401cd61ccfb1ff2
Str1XHyper <50529564+Str1XHyper@users.noreply.github.com>
2021-01-11 17:15:01 +0100

    Added UnitTest Project
    + ShipTests.cs
        + AddContainers_Successfull test
        + ChangeShipSize_Successfull test
        + DistributeContainers_Successfull test
        + DistributeContainers_NotEnoughWeight test

---
+++ ContainerShipTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ContainerShipTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContainerShipTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a651573-1938-47c4-ac61-2e963c734c60")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ContainerShipTests/ShipTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerShipTests
+{
+    [TestClass()]
+    public class ShipTests
+    {
+
+        [TestMethod()]
+        public void AddContainers_Successfull()
+        {
+            //Arrange
+            Ship ship = new Ship(3, 3);
+            List<Container> testContainers = new List<Container>()
+            {
+                new Container(ContainerType.Normal),
+                new Container(ContainerType.Normal),
+                new Container(ContainerType.Cooled),
+                new Container(ContainerType.CooledValueble),
+            };
+
+            //Act
+            ship.AddContainers(testContainers);
+            //Assert
+            Assert.AreEqual(4, ship.Containers.Count);
+        }
+
+        [TestMethod()]
+        public void ChangeShipSize_Successfull()
+        {
+            //Arrange
+            Ship ship = new Ship(3, 3);
+
+            //Act
+            ship.ChangeShipSize(4, 5);
+
+            //Assert
+            Assert.AreEqual(4, ship.Length);
+            Assert.AreEqual(5, ship.Width);
+        }
+
+        [TestMethod()]
+        public void DistributeContainers_Successfull()
+        {
+            //Arrange
+            Ship ship = new Ship(3, 3);
+            List<Container> testContainers = new List<Container>();
+            for(int i = 0; i < 5; i++)
+            {
+                testContainers.Add(new Container(ContainerType.Cooled));
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                testContainers.Add(new Container(ContainerType.Valueble));
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                testContainers.Add(new Container(ContainerType.Normal));
+            }
+
+            //Act
+            ship.AddContainers(testContainers);
+            ship.DistributeContainers();
+        }
+        [TestMethod()]
+        public void DistributeContainers_NotEnoughWeight()
+        {
+            //Arrange
+            Ship ship = new Ship(3, 3);
+            List<Container> testContainers = new List<Container>();
+            for (int i = 0; i < 10; i++)
+            {
+                testContainers.Add(new Container(ContainerType.Normal));
+            }
+
+            //Act
+            ship.AddContainers(testContainers);
+            try
+            {
+                ship.DistributeContainers();
+            } catch (Exception ex)
+            {
+                Assert.AreEqual("There are not enough containers on the ship!", ex.Message);
+            }
+        }
+    }
+}
